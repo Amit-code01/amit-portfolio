e8fc6a7c2df7d5ef83e3b525f6d36d74948f28a8
Amit <amityadavlpms2@gmail.com>
2026-05-11 22:19:57 +0530

    disable email temporarily for testing

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -23,46 +23,46 @@ export const submitContact = async (
 // console.log(process.env.EMAIL_PASS);
 // console.log("BODY:", req.body);
     // Nodemailer Transport
-    const transporter =
-      nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      });
+    // const transporter =
+    //   nodemailer.createTransport({
+    //     service: "gmail",
+    //     auth: {
+    //       user: process.env.EMAIL_USER,
+    //       pass: process.env.EMAIL_PASS,
+    //     },
+    //   });
 
-    // Email Template
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER,
-      subject:
-        "🚀 New Portfolio Contact Message",
+    // // Email Template
+    // await transporter.sendMail({
+    //   from: process.env.EMAIL_USER,
+    //   to: process.env.EMAIL_USER,
+    //   subject:
+    //     "🚀 New Portfolio Contact Message",
 
-      html: `
-        <div style="font-family:Arial;padding:20px;">
-          <h2>New Contact Form Submission</h2>
+    //   html: `
+    //     <div style="font-family:Arial;padding:20px;">
+    //       <h2>New Contact Form Submission</h2>
 
-          <p>
-            <strong>Name:</strong>
-            ${name}
-          </p>
+    //       <p>
+    //         <strong>Name:</strong>
+    //         ${name}
+    //       </p>
 
-          <p>
-            <strong>Email:</strong>
-            ${email}
-          </p>
+    //       <p>
+    //         <strong>Email:</strong>
+    //         ${email}
+    //       </p>
 
-          <p>
-            <strong>Message:</strong>
-          </p>
+    //       <p>
+    //         <strong>Message:</strong>
+    //       </p>
 
-          <div style="background:#f5f5f5;padding:15px;border-radius:8px;">
-            ${message}
-          </div>
-        </div>
-      `,
-    });
+    //       <div style="background:#f5f5f5;padding:15px;border-radius:8px;">
+    //         ${message}
+    //       </div>
+    //     </div>
+    //   `,
+    // });
 
     res.status(201).json({
       success: true,
